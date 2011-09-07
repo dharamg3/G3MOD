674f2672fbe7cc52058220b8ccb42449b764d27b
root <root@ubuntu.(none)>
2011-09-07 04:27:13 -0700

    modified:   Kernel/include/linux/jhash.h 	modified:   initramfs-froyo/init.rc 	initramfs-froyo/lib/modules/tun.ko

--- Kernel/include/linux/autoconf.h
+++ Kernel/include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Tue Aug 30 22:12:31 2011
+ * Wed Sep  7 02:30:38 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_MMC_SDHCI_S3C_DMA 1
