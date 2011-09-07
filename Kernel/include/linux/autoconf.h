59c9f77d0672c281a8db9d67cdb74e1a3776d9de
Dharam Maniar <dharamg3@hotmail.com>
2011-09-07 16:30:12 +0800

    Some Misc changes, may revert back

--- Kernel/include/linux/autoconf.h
+++ Kernel/include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Tue Aug 30 22:12:31 2011
+ * Tue Sep  6 23:39:50 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_MMC_SDHCI_S3C_DMA 1
