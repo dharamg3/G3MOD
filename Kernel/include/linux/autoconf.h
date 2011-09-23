0f96efe3d40f48c899d512e532824d6a62108223
Miki Dahab <moikop@live.com>
2011-09-22 22:19:35 -0300

    Some misc changes to 'Lowmemorykiller.c

--- Kernel/include/linux/autoconf.h
+++ Kernel/include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Wed Sep 21 22:13:33 2011
+ * Thu Sep 22 22:00:35 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_MMC_SDHCI_S3C_DMA 1
