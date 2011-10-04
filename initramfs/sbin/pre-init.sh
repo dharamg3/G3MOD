5eec208fd3c4afe34d09dd569fe8defba75a6a05
Miki Dahab <moikop@live.com>
2011-10-03 21:52:55 -0300

    Fixed some things related with Data2SD

--- initramfs/sbin/pre-init.sh
+++ initramfs/sbin/pre-init.sh
@@ -441,9 +441,6 @@ then
 		echo "Data2SD Enabled - Hybrid Mode" >> /data2sd.log	
 		echo "Data2SD Enabled - Hybrid Mode" >> /g3mod.log
 		sed -i "s|g3_mount_stl7|# Line not needed for Hybrid Data2SD|" /init.rc /recovery.rc
-
-		cp /system/etc/data2sd.dirs /
-		cp $G3DIR/data2sd.dirs /
 	else
 		echo "Data2SD Enabled - Standard Mode" >> /data2sd.log	
 		echo "Data2SD Enabled - Standard Mode" >> /g3mod.log
@@ -451,6 +448,10 @@ then
 		mount -o bind /sdext /data
 		sed -i "s|g3_mount_stl7|mount ${MMC_FS} /dev/block/mmcblk0p2 /data noatime nodiratime nosuid nodev rw|" /init.rc /recovery.rc
 	fi
+	
+	cp /system/etc/data2sd.dirs /
+	cp $G3DIR/data2sd.dirs /
+	
 else
 	echo "Data2SD Disabled" >> /g3mod.log
 	sed -i "s|g3_mount_stl7|mount ${STL7_FS} /dev/block/stl7 /data noatime nodiratime nosuid nodev rw ${STL7_MNT}|" /init.rc /recovery.rc
@@ -517,7 +518,7 @@ if test -f /data2sd.dirs; then
 	echo "Connecting Hybrid Data2SD Links" >> /data2sd.log
 	cat /data2sd.dirs | while read line
 	do
-		DATA2SDtemp="${line%?}"
+		DATA2SDtemp="$line"
 
 		cp -prf /intdata/$DATA2SDtemp /sdext/
 		mkdir /sdext/$DATA2SDtemp
@@ -530,7 +531,7 @@ if test -f /data2sd.dirs; then
 
 
 else
-	echo "No Data2SD config file found (/system/etc/data2sd.dirs or /sdcard/Android/data/g3mod/data2sd.dirs" >> /data2sd.log
+	echo "No Data2SD config file found (/system/etc/data2sd.dirs or /sdcard/Android/data/g3mod/data2sd.dirs)" >> /data2sd.log
 fi
 
 rm /data2sd.dirs
