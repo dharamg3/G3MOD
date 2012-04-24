05a2d57b10e346f3fd0373737bdf07d907c92a2d
Mark Kennard <komcomputers@gmail.com>
2012-04-25 11:38:15 +1200

    New build.sh from Kyorakernel

--- Kernel/build.sh
+++ Kernel/build.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
-make apollo_rev_02_android_defconfig
-make -j8
-cd modules/vibetonz
-make KDIR=../../
-cd ../fm_si4709
-make KDIR=../../
+
+echo "Generating initramfs.cpio"
+cd ../initramfs
+find . -print0 | cpio --null -ov --format=newc > ../initramfs.cpio
+cd ../Kernel
+
+if [ -z $1 ]; then
+	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
+else
+	echo "Setting kernel name to ($1)"
+	export KBUILD_BUILD_VERSION=$1
+fi
+
+echo "Compiling the kernel"
+make -j2
+
+echo "Tarballing the kernel"
+cp arch/arm/boot/zImage ./
+tar cf $KBUILD_BUILD_VERSION-zImage.tar zImage
+rm zImage
+
+echo "Done"
